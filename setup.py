8fdeb89080bf1e138c70f7e4690b95647663e606
Manpreet Bahl <manpreetsingh.bahl@gmail.com>
2019-02-12 15:43:37 -0800

    Updated setup

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
 from setuptools import setup
+
+def get_requirements():
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')) as f:
+        return f.read()
 
 setup(
    name='eight-puzzle-manpreet',
@@ -7,7 +12,7 @@ setup(
    author='Manpreet Bahl',
    author_email='manpreetsingh.bahl@gmail.com',
    python_requires='>=3.5.0',
-   py_modules=['eight-puzzle-manpreet.py'],
-   install_requires=['numpy', 'scipy'],
-   scripts=['bin/eight-puzzle-manpreet.py']
+   packages=['eight-puzzle-manpreet'],
+   install_requires=get_requirements(),
+   scripts=['bin/eight-puzzle-manpreet']
 )
